69ce5a42fbee24c624da85bfe9b49e605ba15a3b
Peter Andrén <piddah80@gmail.com>
2024-11-21 10:19:42 +0100

    "First Commit"

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+ProductPresentation(ProductBackend());
+
+
+
+string ProductBackend()
+{
+    string[] products = ["Milk", "Yoghurt", "Cream", "Cottage Cheese"];
+    string stringOfProducts = "";
+
+    foreach (string product in products)
+    {
+        stringOfProducts += product + " ";
+    }
+    return stringOfProducts;
+}
+
+void ProductPresentation(string product)
+{
+    string[] productsArray = product.Split(" ");
+
+    foreach (string p in productsArray)
+    {
+        Console.WriteLine(p);
+    }
+}
